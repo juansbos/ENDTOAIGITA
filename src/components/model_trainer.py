b96771f1e26e88fb3a40eecbad7008ca291fe90e
rosco <soyjuan02@outlook.com>
2026-03-09 10:34:29 -0600

    3 left

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,25 +43,62 @@ class ModelTrainer:
                 "Decision Tree": DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                # ✅ Fix 3: nombre corregido de KNN
-                "KNN Regressor": KNeighborsRegressor(),
-                # ✅ Fix 4: nombres corregidos a Regressor
+                "K_Neighbour Regressor": KNeighborsRegressor(),
                 "XGB Regressor": XGBRegressor(),
-                "CatBoost Regressor": CatBoostRegressor(verbose=False),  # ✅ Fix 5: verbose correcto
+                "CatBoost Regressor": CatBoostRegressor(verbose=False), 
                 "AdaBoost Regressor": AdaBoostRegressor()
+            }
+            params = {
+                "Decision Tree": { 
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    #'splitter' : ['best','random'],
+                    #'max_features':['sqrt','log2'],
+                },
+                "Random Forest" : {
+                    # 'criterion':['squared_error','friedman_mse','absolute_error','´poisson'],
+                    # 'max_features':['sqrt'.'log2',Nne],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    #'loss':['squared_error','huber','absolute_error','quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    #'criterion':['squared_error','friedman_mse'],
+                    #'max_features':['auto','sqrt','log2'],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K_Neighbour Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    # 'weights':['uniform','distance'],
+                    #'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGB Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoost Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
             }
 
             model_report: dict = evaluate_models(
                 x_train=x_train, y_train=y_train,
                 x_test=x_test, y_test=y_test,
-                models=models
+                models=models,param=params
             )
 
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            # ✅ Fix 6: usar best_model_name como key, no best_model_score
             best_model = models[best_model_name]
 
             if best_model_score < 0.6:
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+'''
 import sys
 import os
 
@@ -6,6 +7,7 @@ import pandas as pd
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 import dill
 
@@ -18,14 +20,22 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+    
+def evaluate_models(x_train, y_train, x_test, y_test, models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train, y_train)
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            #gs = GridSearchCV(model,para,cv=cv,n_jobs=n_jobs,verbose=verbose,refit=refit)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            #model.fit(x_train, y_train)
 
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
@@ -36,5 +46,72 @@ def evaluate_models(x_train, y_train, x_test, y_test, models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+        '''
+
+import sys
+import os
+
+import numpy as np
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
+from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from catboost import CatBoostRegressor
+
+import dill
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(x_train, y_train, x_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param[model_name]
+
+            # CatBoost is incompatible with sklearn's GridSearchCV in newer sklearn versions.
+            # Use CatBoost's native grid_search instead.
+            if isinstance(model, CatBoostRegressor):
+                if para:
+                    model.grid_search(para, x_train, y_train, cv=3, verbose=0)
+                else:
+                    model.fit(x_train, y_train)
+            else:
+                if para:
+                    gs = GridSearchCV(model, para, cv=3)
+                    gs.fit(x_train, y_train)
+                    model.set_params(**gs.best_params_)
+                model.fit(x_train, y_train)
+
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
