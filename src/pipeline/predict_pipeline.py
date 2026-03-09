52546beedbeb0092747cbc9bf43bf019bd8f6ebc
rosco <soyjuan02@outlook.com>
2026-03-09 14:53:05 -0600

    test deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import os
 from src.exception import CustomException
 from src.utils import load_object
 
@@ -9,18 +10,24 @@ class PredictPipeline:
 
     def predict(self, features):
         try:
-            model_path = 'artifacts/model.pkl'
-            preprocessor_path = 'artifacts/preprocessor.pkl'
+            ###model_path = 'artifacts/model.pkl'
+            ###preprocessor_path = 'artifacts/preprocessor.pkl'
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            print("Before Loading")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
+            print("Predictions made")
             return preds
+        
         except Exception as e:
             raise CustomException(e, sys)
         
 class CustomData:
-    def __init__(selfl,
+    def __init__(self,
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
@@ -54,6 +61,10 @@ class CustomData:
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score]
             }
+
             return pd.DataFrame(custom_data_input_dict)
+        
         except Exception as e:
             raise CustomException(e, sys)
+        
+        
